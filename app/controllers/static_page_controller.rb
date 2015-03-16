fa0217f1e4b740947b916b816111dbbaa88fca62
Landon Adrian <landon.s.adrian@gmail.com>
2015-03-16 19:37:22 -0400

    splash/landing page and the modal added, a work in progress

--- app/controllers/static_page_controller.rb
+++ app/controllers/static_page_controller.rb
@@ -3,4 +3,15 @@ class StaticPageController < ApplicationController
 	def show
 		render params[:id] rescue render 'home'
 	end
+
+	def splashpage
+		if user_signed_in?
+			render 'home'
+			return
+		else
+
+		render :action => "splashpage", :layout => "splashpage"
+		return
+	end
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
 
   get '/:id', to: 'static_page#show', as: :spage
   
-  root "static_page#show"
+  root "static_page#splashpage"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
